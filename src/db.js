540138ff1ddf4e65db6512649ead3167b4b8a25e
jetminfo <jetm.info@gmail.com>
2025-08-26 12:54:09 +0200

    Backup Working - All Browser no crash

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ export const db = new Dexie('cs2TradeUpDB');
 db.version(7).stores({
   inventory: '++id,name,wear,float,collection,collectionIMGUrl,rarity,isStatTrak,imageUrl',
   allSkins: '++id,name,wear,rarity,isStatTrak,isSouvenir,isST,isSV,collection,price,date,volume,imageUrl',
-  history: '++id,date,name,wear,float,rarity,isStatTrak,isSouvenir,isST,isSV,collection,price,date,volume',
+  history: '++id,date,name,wear,float,rarity,isStatTrak,isSouvenir,isST,isSV,collection,price,volume',
   currentTradeUps: '++id,name,collection,inputs,outputs,resultSkin,isStatTrak,profitability,date,urls',
   savedTradeUps: '++id,name,collection,inputs,outputs,resultSkin,isStatTrak,profitability,date,urls',
   favoriteTradeUps: 'id' // ⭐ Table des favoris
